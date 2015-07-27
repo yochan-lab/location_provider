4d12477283ba0c0a645340fab65c6f215e8b6523
Jake Merdich <jake@merdich.com>
2015-07-27 09:17:39 -0700

    cleanup and added GetLocationList service

--- scripts/location_provider_node.py
+++ scripts/location_provider_node.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 __author__ = 'daniel'
-
+import traceback
 import rospy
 from location_provider.srv import *
 
 import rosplan_interface as interface
 
-from geometry_msgs.msg import PoseWithCovariance, Pose, Point,\
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point,\
     Quaternion
 from move_base_msgs.msg import *
 from actionlib import *
+from threading import Thread
 
-pose = PoseWithCovariance(Pose(Point(0, 0, 0),
+pose = PoseWithCovarianceStamped(Pose(Point(0, 0, 0),
                                Quaternion(0, 0, 0, 1)),
                           [0.0]*36)
 
@@ -61,12 +62,11 @@ pose = PoseWithCovariance(Pose(Point(0, 0, 0),
 #     return output
 
 
-def roam():
-    for location in interface.list_instances('location'):
-        interface.add_goal('hasvisited', x=location)
-    interface.add_instance('location', 'starting_point')
-    interface.add_predicate('robotat', x='starting_point')
-    interface.plan()
+#def roam():
+#    for location in interface.list_instances('location'):
+#        interface.add_goal('hasvisited', x=location)
+#
+#    interface.plan()
 
 
 def set_location_function(name, pose_in=None):
@@ -79,7 +79,7 @@ def set_location_function(name, pose_in=None):
     # interface.add_instance('location', name, pose)
 
 
-def update_pose_function(arg):
+def update_pose_function(arg, *args, **kwargs):
     global pose
     pose = arg
 
@@ -89,46 +89,78 @@ class moveAction(interface.Action):
     def __init__(self, *args, **kwargs):
         self.client = SimpleActionClient('move_base', MoveBaseAction)
         self.client.wait_for_server(rospy.Duration(secs=10))
+        self.cancelled = False
         super(moveAction, self).__init__(*args, **kwargs)
+        print "DONE WITH INIT"
 
     def start(self, loca, locb):
+        self.cancelled = False
         goal = MoveBaseActionGoal()
-        goal.goal.target_pose = interface.get_instance(locb, Pose._type)
-        print goal.goal.target_pose
-        client.send_goal(goal)
-        client.wait_for_result()
-        if client.get_result() != GoalStatus.SUCCEEDED:
-            raise Exception("Moving failed!")
+        goal.goal.target_pose.pose = interface.get_instance('location', locb, Pose._type)[0]
+        goal.goal.target_pose.header.frame_id = "map"
+        print goal.goal.target_pose.pose
+        self.client.send_goal(goal.goal)
+        Thread(target=self.bkgd).start() 
+    
+    def bkgd(self):
+        self.client.wait_for_result()
+        if not self.cancelled:
+	    self.finish()
+
+    def finish(self):
+        interface.rm_predicate('robotat', x=self.arguments['loca'])
+        interface.add_predicate('robotat', x=self.arguments['locb'])
+        interface.add_predicate('hasvisited', x=self.arguments['locb'])
+        self.report_success()
+#         if self.client.get_result() != GoalStatus.SUCCEEDED:
+#             print self.client.get_result()
+#             raise Exception("Moving failed!")
 
     def cancel(self):
-        client.cancel_goal()
+        print "cancelling goal"
+        self.cancelled = True
+	self.client.cancel_goal()
+	print "goal canceled"
 
 
-locations = {"tony_office": Pose(Point(2.495, 1.964, 0.000), Quaternion(0.000, 0.000, -0.044, 0.999)),
-             "room_five_sixty": Pose(Point(2.581, 4.985, 0.000), Quaternion(0.000, 0.000, -0.028, 1.000)),
-             "kitchen": Pose(Point(-8.588, 28.927, 0.000), Quaternion(0.000, 0.000, 1.000, -0.015)),
-             "cubicle": Pose(Point(-0.717, 0.897, 0.000), Quaternion(0.000, 0.000, 0.734, 0.680)),
-             "conference_room": Pose(Point(1.056, 35.795, 0.000), Quaternion(0.000, 0.000, 0.048, 0.999)),
-             "it": Pose(Point(-15.275, 36.182, 0.000), Quaternion(0.000, 0.000, 0.741, 0.672)),
-             "entrance": Pose(Point(-4.926, 5.199, 0.000), Quaternion(0.000, 0.000, 0.977, -0.211))}
+locations = {"home": Pose(Point(-1.395, 1.091, 0.000), Quaternion(0.000, 0.000, 0.701, 0.713)),
+	"office": Pose(Point(2.237, 2.296, 0.000), Quaternion(0.000, 0.000, -0.038, 0.999)),
+	"rao": Pose(Point(2.281, 5.228, 0.000), Quaternion(0.000, 0.000, 0.006, 1.000)),
+	"monica": Pose(Point(-1.813, -2.504, 0.000), Quaternion(0.000, 0.000, -0.347, 0.938)),
+	"entrance": Pose(Point(-5.286, 5.447, 0.000), Quaternion(0.000, 0.000, 0.992, -0.127)),
+	"bathroom": Pose(Point(-4.848, 17.113, 0.000), Quaternion(0.000, 0.000, 1.000, -0.002)),
+	"fred": Pose(Point(-0.586, 19.598, 0.000), Quaternion(0.000, 0.000, -0.725, 0.689)),
+	"kitchen": Pose(Point(-14.417, 28.528, 0.000), Quaternion(0.000, 0.000, 0.000, 1.000)),
+	"classroom": Pose(Point(-23.638, 19.286, 0.000), Quaternion(0.000, 0.000, 0.992, 0.128)),
+	"brint": Pose(Point(-22.049, 39.687, 0.000), Quaternion(0.000, 0.000, 0.721, 0.693)),
+	"IT": Pose(Point(-21.433, 37.345, 0.000), Quaternion(0.000, 0.000, -0.034, 0.999)),
+	"conferenceroom": Pose(Point(3.326, 38.098, 0.000), Quaternion(0.000, 0.000, 0.723, 0.691)),
+	"chattin": Pose(Point(-26.513, -7.674, 0.000), Quaternion(0.000, 0.000, -0.717, 0.697))}
+
+def handle_location_list(req):
+	return GetLocationListResponse(locations.keys())
 
 if __name__ == "__main__":
     rospy.init_node("location_provider")
-    interface.init_rosplan()
+    # interface.init_rosplan()
 #     a = rospy.Service(OB'get_location_from_pose',
 #                       getLocationFromPose,
 #                       get_location_from_pose_function)
-#     b = rospy.Service('get_location_list',
-#                       getLocationList,
-#                       get_location_list_function)
-    c = rospy.Service('setLocation',
-                      setLocation,
-                      set_location_function)
+    b = rospy.Service('get_location_list',
+                       GetLocationList,
+                       handle_location_list)
+#    c = rospy.Service('setLocation',
+#                      setLocation,
+#                      set_location_function)
     amcl_pose = rospy.Subscriber('amcl_pose',
-                                 PoseWithCovariance,
+                                 PoseWithCovarianceStamped,
                                  update_pose_function, 1, 1)
-
-    for i in locations:
-        set_location_function(i, locations[i])
-    roam()
+    interface.init_rosplan()
+    #for i in locations:
+    #    set_location_function(i, locations[i])
+#
+ #   interface.add_instance('location', 'starting_point')
+  #  interface.add_predicate('robotat', x='starting_point')
+    
+    #roam()
     rospy.spin()
